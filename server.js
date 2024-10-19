88097faf57a229d4562bf5f872164acd362e6397
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-10-19 20:59:06 +0700

    tambah gallery

--- server.js
+++ server.js
@@ -14,5 +14,9 @@ app.get("/home", async (req, res) => {
     res.send('<h3>Home</h3>');
 });
 
+app.get("/gallery", async (req, res) => {
+    res.send('<h3>Gallery</h3>');
+});
+
 
 app.listen('4000');
