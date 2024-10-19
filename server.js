12bfc52b985b8acd36317adf1f3bba95b8a5c21f
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-10-19 20:54:09 +0700

    update pertama

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", async (req, res) => {
+    res.send('<h3>Hallo</h3>');
+});
+
+app.get("/about", async (req, res) => {
+    res.send('<h3>About</h3>');
+});
+
+
+app.listen('4000');
