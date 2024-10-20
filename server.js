296483e2add37f5702f0dd8700a9fa6c14932b34
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-10-20 11:41:22 +0700

    update 11.41

--- server.js
+++ server.js
@@ -4,23 +4,19 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get("/", async (req, res) => {
-    res.send('<h3>Menu : <a href="/about"> about </a> | <a href="/home"> home </a> | <a href="/gallery"> gallery </a> | <a href="/video"> video </a></h3>');
+    res.sendFile(path.resolve('./views/index.html'));
 });
 
 app.get("/about", async (req, res) => {
     res.sendFile(path.resolve('./views/about.html'));
 });
 
-app.get("/home", async (req, res) => {
-    res.send('<h3>Home</h3>');
-});
-
 app.get("/gallery", async (req, res) => {
-    res.send('<h3>Gallery</h3>');
+    res.sendFile(path.resolve('./views/gallery.html'));
 });
 
 app.get("/video", async (req, res) => {
-    res.send('<h3>Video</h3>');
+    res.sendFile(path.resolve('./views/video.html'));
 });
 
 
