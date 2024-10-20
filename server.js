146df2c9c54eecac9c09239f9c0aa70e7ce048ce
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-10-20 11:26:41 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-
+app.use(express.static('public'));
 
 app.get("/", async (req, res) => {
     res.send('<h3>Menu : <a href="/about"> about </a> | <a href="/home"> home </a> | <a href="/gallery"> gallery </a> | <a href="/video"> video </a></h3>');
